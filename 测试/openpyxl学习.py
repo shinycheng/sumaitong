0171f4b071ab1b529350a165830d5974fbe6a3d0
shinycheng <664342094@qq.com>
2020-11-18 22:59:34 +0800

    1

---
+++ 测试/openpyxl学习.py
@@ -0,0 +1,14 @@
+from openpyxl import Workbook
+wb = Workbook()
+ws = wb.active
+ws1 = wb.create_sheet("Mysheeet")
+ws.title = "New"
+ws3 = wb["New"]
+c = ws['A4']
+ws['a4'] = 4
+d = ws.cell(row=4,column=2,value=10)
+for i in range(1,10):
+    for y in range(1,101):
+        ws.cell(row=i,column=y,value=y)
+wb.save('pyxl.xlsx')
+print(ws['A1':'C2'])
